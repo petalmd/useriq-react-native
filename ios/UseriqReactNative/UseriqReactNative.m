995f62e4e6387e6c24f395100ac80cf0c539d3fa
aravindgs <aravindgs007@gmail.com>
2019-02-25 22:50:44 +0530

    Added promise blocks to `showCtxHelp` and `showHelpCentre`

--- ios/UseriqReactNative/UseriqReactNative.m
+++ ios/UseriqReactNative/UseriqReactNative.m
@@ -15,46 +15,65 @@ RCT_EXPORT_MODULE(UseriqReactNative);
 RCT_EXPORT_METHOD(init:(NSString *)apiKey){
     self.apiKey = apiKey;
     RCTLogInfo(@"\n*****SDK initialized with apiKey: %@*****",apiKey);
-    resolve(@"successful UserIQ SDK initialization");
+    //    resolve(@"successful UserIQ SDK initialization");
 }
 
-RCT_EXPORT_METHOD(setUser:(NSString *)userId
-                  name:(NSString *)name
-                  email:(NSString *)email
-                  accountId:(int)accId
-                  accountName:(NSString *)accName
-                  signupDate:(NSString *)signupDate
-                  andParameters:(NSDictionary *)parameters) {
-    RCTLogInfo(@"\n*****SDK setUser is successful with apiKey: %@\nuserId: %@\nname: %@\nemail: %@\naccount Id: %d\naccount Name: %@\nsignup Date: %@\nparameters: %@*****",self.apiKey,userId,name,email,accId,accName,signupDate,parameters);
+RCT_EXPORT_METHOD(setUser:(NSDictionary *)user){
+    RCTLogInfo(@"%@",user);
     NSString *apiKeyMissing = @"apiKey Missing";
     NSAssert((![self.apiKey isEqualToString:@""]&&self.apiKey), apiKeyMissing);
+    NSString *userId, *name, *email, *accountName, *signupDate;
+    int accId;
+    NSMutableDictionary *parameters = [[NSMutableDictionary alloc] init];
+    for (NSString *key in [user allKeys]) {
+        if ([key isEqualToString:@"id"]) {
+            userId = [user valueForKey:@"id"];
+        } else if([key isEqualToString:@"name"]) {
+            name = [user valueForKey:@"name"];
+        } else if([key isEqualToString:@"email"]) {
+            email = [user valueForKey:@"email"];
+        } else if([key isEqualToString:@"accountId"]) {
+            accId = [[user valueForKey:@"accountId"] intValue];
+        } else if([key isEqualToString:@"accountName"]) {
+            accountName = [user valueForKey:@"accountName"];
+        } else if([key isEqualToString:@"signUpDate"]) {
+            signupDate = [user valueForKey:@"signUpDate"];
+        } else {
+            [parameters setObject:[user valueForKey:key] forKey:key];
+        }
+    }
     [[UserIQSDK sharedInstance] initWithAPIKey:self.apiKey
                                         userId:userId
                                           name:name
                                          email:email
                                      accountId:accId
-                                   accountName:accName
+                                   accountName:accountName
                                     signupDate:signupDate
                                  andParameters:parameters];
-    resolve(@"successful UserIQ SDK setUser Successful");
+    //    resolve(@"successful UserIQ SDK setUser Successful");
 }
 
 RCT_EXPORT_METHOD(disableFAB) {
     [[UserIQSDK sharedInstance] disableFAB];
 }
 
-RCT_REMAP_METHOD(showCtxHelp, resolve:(RCTPromiseResolveBlock)resolve){
-    BOOL shown = [[UserIQSDK sharedInstance] showCtxHelp];
-    resolve([NSNumber numberWithBool:shown]);
-}
-
-RCT_EXPORT_METHOD(showHelpCentre){
+RCT_REMAP_METHOD(showHelpCentre,
+                 resolve:(RCTPromiseResolveBlock)resolve
+                 reject:(RCTPromiseRejectBlock)reject){
     [[UserIQSDK sharedInstance] showHelpCentre];
+    resolve(@"true");
 }
 
 RCT_EXPORT_METHOD(setHost:(NSString *)host) {
     [[UserIQSDK sharedInstance] setHost:host];
 }
 
+RCT_REMAP_METHOD(showCtxHelp,
+                 resolver:(RCTPromiseResolveBlock)resolver
+                 rejecter:(RCTPromiseRejectBlock)rejecter) {
+    BOOL shown = [[UserIQSDK sharedInstance] showCtxHelp];
+    resolver((shown?@"true":@"false"));
+}
+
 @end
-  
+
